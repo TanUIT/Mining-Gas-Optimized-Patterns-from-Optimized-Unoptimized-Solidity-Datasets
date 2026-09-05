f5e8bd90c797902df175d61df8e0f60e0e102ae7
TanUIT <tannh.19@grad.uit.edu.vn>
2026-09-05 15:09:54 +0700

    Delete dataset directory

--- dataset/007_external_visibility/optimized.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Api {
-    function ping(uint256 x) external pure returns (uint256) {
-        return x + 1;
-    }
-}
--- dataset/009_rename_only/optimized.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Adder {
-    function compute(uint256 a, uint256 b) external pure returns (uint256) {
-        uint256 c = a + b;
-        return c;
-    }
-}
